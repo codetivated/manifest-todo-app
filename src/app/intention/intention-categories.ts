3abf0f5cd427d7c88bf8c986c95321d28414c34f
codetivated <melisa@heying.us>
2025-08-06 15:41:06 -0500

    update how intentions page looks

--- src/app/intention/intention-categories.ts
+++ src/app/intention/intention-categories.ts
@@ -3,15 +3,18 @@ export const INTENTION_CATEGORIES = [
     id: 'mind_intention',
     category: 'Mind',
     icon: 'mind-intention-icon.png',
+    intentions: [],
   },
   {
     id: 'body_intention',
     category: 'Body',
     icon: 'body-intention-icon.png',
+    intentions: [],
   },
   {
     id: 'soul_intention',
     category: 'Soul',
     icon: 'soul-intention-icon.png',
+    intentions: [],
   },
 ];
--- src/app/intention/intention.component.ts
+++ src/app/intention/intention.component.ts
@@ -1,20 +1,41 @@
 import { Component } from '@angular/core';
 import {INTENTION_CATEGORIES} from './intention-categories';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-intention',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './intention.component.html',
   styleUrl: './intention.component.css'
 })
 
 export class IntentionComponent {
-  intentions: any = INTENTION_CATEGORIES;
+  intentions: any[] = [];
+  selectedCategory: any = null;
+  newIntention: string = '';
 
   constructor() {
+    this.intentions = INTENTION_CATEGORIES.map((intention) => ({
+      ...intention,
+      imagePath: '../../assets/intentions/' + intention.icon
+    }));
     console.log(this.intentions);
   }
 
+
+  onSelectCategory(category: any) {
+    this.selectedCategory = category;
+    this.newIntention = '';
+  }
+
+  addIntention() {
+    if (this.newIntention.trim()) {
+      this.selectedCategory.intentions.push(this.newIntention.trim());
+      this.newIntention = '';
+    }
+  }
 }
+
+
