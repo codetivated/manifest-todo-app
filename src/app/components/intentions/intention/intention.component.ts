25d738d8053fba8536faa2f229a775ba8aca1ff4
codetivated <melisa@heying.us>
2025-09-02 10:21:14 -0500

    update intention css and add pipe

--- src/app/components/intentions/intention/intention.component.ts
+++ src/app/components/intentions/intention/intention.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Intention } from '../../../models/intention.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-intention',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './intention.component.html',
   styleUrl: './intention.component.css'
 })
